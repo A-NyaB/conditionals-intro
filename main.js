04407d8e01bf48ca2a946af90c4c0c4c5401943f
A-Nya Badger <asbadger01@gmail.com>
2023-08-30 13:35:57 -0400

    These conditionals are conditionally mastered

---
+++ main.js
@@ -0,0 +1,52 @@
+if(1+1 > 5){
+    console.log("True fact!")
+} else {
+    console.log("LIES!")
+}
+
+if(4*5 <= 20){
+    console.log("True fact!")
+} else {
+    console.log("LIES!")
+}
+
+if(6-2 >= 0){
+    console.log("True fact!")
+} else {
+    console.log("LIES!")
+}
+
+
+let likesDogs = true;
+   if (likesDogs === true){
+    console.log("You're a dog person!")
+   } else if (likesDogs === false){
+    console.log("You're a cat person!")
+   }
+
+
+let NewFriend = {
+    name: "Amy",
+    preferredLanguage: "French"
+}
+
+if (NewFriend.preferredLanguage === "English"){
+    console.log(`Hello, ${NewFriend.name}!`);
+} else if (NewFriend.preferredLanguage === "Spanish"){
+    console.log(`Hello, ${NewFriend.name}!`);
+} else if (NewFriend.preferredLanguage === "French"){
+    console.log(`Hello, ${NewFriend.name}!`);}
+
+  let grade = 92  
+
+  if(grade >= 0 && grade <= 69){
+    console.log("You got an F")
+  } else if (grade >= 70 && grade <= 76){
+    console.log("You got a D")
+  } else if (grade >= 77 && grade <= 84){
+    console.log("You got a C")
+  } else if (grade >= 84 && grade <= 92){
+    console.log("You got a B")
+  } else if (grade >= 93 && grade <= 100){
+    console.log("You got an A")
+  }
